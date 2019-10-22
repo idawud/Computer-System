fa01011c1aa3c6f83c379fe07334f3111149e36d
idawud <ismaildawud96@gmail.com>
2019-10-22 13:32:11 +0000

    word count program

--- 1.wordCount/wc.C
+++ 1.wordCount/wc.C
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <cstring>
 #include <string>
+#include <cstring>
 #include <unistd.h>
 #include <fcntl.h>
 
-#define BUFSIZE 1025
-
+using namespace std;
 int main() {
 	int bytes = 0, words = 0, lines = 0;
 	int fd = open("wcreport", O_RDWR | O_CREAT, 0644);
@@ -14,28 +13,28 @@ int main() {
 		perror("Result");
 		exit(1);
 	}
-
-	char buffer[BUFSIZE];
-	ssize_t length;
-	while (((length = read(0, buffer, BUFSIZE)) > 0)  && (bytes < BUFSIZE)) {
-	 	// write(1, buffer, length); 
-		bytes  += length;
-		
-		for( char ch : buffer){
-			if ( ch == ' ') {
-				words += 1;
-			}else if (ch == '\n'){
-				// words += 1;
-				lines += 1;
-			}
+	
+	string content, sentence;
+	do{
+		cout << ">>> ";
+		getline(cin, sentence);
+		if( !sentence.empty()){	
+			content += sentence + "\n";
+			++lines;
 		}
+	}while( !sentence.empty());
 		
-		std::string report = "bytes: " + std::to_string(bytes);
-		report += "\nwords: " + std::to_string(words);
-		report += "\nlines: " + std::to_string(lines) + "\n";
- 
-		write(fd, report.c_str(), std::strlen(report.c_str()));
-		close(fd);
-	 
-	}
+	std::string report = "bytes: " + std::to_string(content.length());
+	report += "\nlines: " + std::to_string(lines);
+	
+	for( auto ch : content){
+		if( ch == ' ' || ch == '\n'){
+			++words;
+		}
+	}	
+ 	
+	report += "\nwords: " + std::to_string(words) + "\n";
+
+	write(fd, report.c_str(), std::strlen(report.c_str()));
+	close(fd); 
 }
