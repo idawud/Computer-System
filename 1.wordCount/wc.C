584f2eeada0b3b87f5c06d3d7ad64c4a730006a9
idawud <ismaildawud96@gmail.com>
2019-10-25 06:56:02 +0000

    updated wc.C

--- 1.wordCount/wc.C
+++ 1.wordCount/wc.C
@@ -3,11 +3,23 @@
 #include <cstring>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h> 
 
 using namespace std;
 int main() {
 	int bytes = 0, words = 0, lines = 0;
-	int fd = open("wcreport", O_RDWR | O_CREAT, 0644);
+
+	int fd;
+	if ( mkdir("wcreports", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH ) == 0){
+		fd = open("wcreports/wcreport", O_RDWR | O_CREAT, 0644);
+	}
+	else{
+		cerr << "Error :  wcreports directory not created" << endl; 
+		exit(-1);
+	}
+	
+		
 
 	if ( fd < 0 ) {
 		perror("Result");
